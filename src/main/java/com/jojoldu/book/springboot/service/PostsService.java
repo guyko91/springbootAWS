9307c4bfb6db035c5a3ef1af69190faaf119ff9d
guyko91 <guyko91@gmail.com>
2021-01-09 22:38:01 +0900

    12월 9일 (~ CRUD 기본 템플릿 완료)

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.springboot.service;
 
 import com.jojoldu.book.springboot.web.domain.posts.Posts;
 import com.jojoldu.book.springboot.web.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +37,18 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선됨.
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
 
 }
--- src/main/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepository.java
@@ -1,7 +1,16 @@
 package com.jojoldu.book.springboot.web.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // Mybatis 에서 dao로 불리는 DB Layer 접근자 = JPA Repository
 // JpaRepository<Entity 클래스, PK 타입>
-public interface PostsRepository extends JpaRepository<Posts, Long> {}
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
+
+}
